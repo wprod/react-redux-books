57390c58a3993bb88ae9314a586926106179b0c8
witters <witters@novius.fr>
2017-09-04 14:02:36 +0200

    first hour

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function() {
+    return [
+        { title: 'Js, the good part'},
+        { title: 'Rails, the bad part'},
+        { title: 'Php, the meh part'},
+        { title: 'Css, the needed part'}
+    ]
+}
